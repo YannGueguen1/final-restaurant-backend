647d187db43238ee65acac9fb778dcf27524b5ba
YannGueguen1 <yanngueguen2015@u.northwestern.edu>
2022-04-16 16:33:17 -0500

    second commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'edcb569638d7c94f897cab30b9f0817b'),
+    },
+  },
+});
