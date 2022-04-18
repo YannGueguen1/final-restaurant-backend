08d66c3920474b9f59a1164d79901eee3ae7c9b6
YannGueguen1 <yanngueguen2015@u.northwestern.edu>
2022-04-18 11:18:40 -0500

    resolved post strapi issue

--- api/order/controllers/Order.js
+++ api/order/controllers/Order.js
@@ -15,29 +15,58 @@ module.exports = {
    * @return {Object}
    */
 
-  create: async (ctx) => {
-    const { address, amount, dishes, token, city, state } = JSON.parse(
-      ctx.request.body
-    );
+  // create: async (ctx) => {
+  //   const { address, amount, dishes, token, city, state } = JSON.parse(
+  //     ctx.request.body
+  //   );
+  //   console.log(ctx)
+  //   console.log(ctx.state)
+  //   const stripeAmount = Math.floor(amount * 100);
+  //   // charge on stripe
+  //   const charge = await stripe.charges.create({
+  //     // Transform cents to dollars.
+  //     amount: stripeAmount,
+  //     currency: "usd",
+  //     description: `Order ${new Date()} by ${ctx.state.user._id}`,
+  //     source: token,
+  //   });
+
+  //   // Register the order in the database
+  //   const order = await strapi.services.order.create({
+  //     user: ctx.state.user.id,
+  //     charge_id: charge.id,
+  //     amount: stripeAmount,
+  //     address,
+  //     dishes,
+  //     city,
+  //     state,
+  //   });
+
+  //   return order;
+  // },
+  create: async ctx => {
+    const { address, amount, dishes, token, city, state } = JSON.parse(ctx.request.body);
+
     const stripeAmount = Math.floor(amount * 100);
-    // charge on stripe
+
     const charge = await stripe.charges.create({
       // Transform cents to dollars.
       amount: stripeAmount,
       currency: "usd",
       description: `Order ${new Date()} by ${ctx.state.user._id}`,
-      source: token,
+      source: token
     });
 
     // Register the order in the database
     const order = await strapi.services.order.create({
-      user: ctx.state.user.id,
-      charge_id: charge.id,
-      amount: stripeAmount,
+      user: ctx.state.user._id,
+      // user: "john",
       address,
+      amount: stripeAmount,
       dishes,
       city,
       state,
+      charge_id: charge.id
     });
 
     return order;
