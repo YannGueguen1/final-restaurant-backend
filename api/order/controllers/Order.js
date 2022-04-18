cdc59aaa3555b4a4ddc275ae054c486a4431aecd
YannGueguen1 <yanngueguen2015@u.northwestern.edu>
2022-04-18 01:06:55 -0500

    latest backend

--- api/order/controllers/Order.js
+++ api/order/controllers/Order.js
@@ -1,107 +1,45 @@
-
-'use strict';
-
+"use strict";
 /**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
+ * Order.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Order`.
  */
-
-const stripe = require("stripe")("sk_test_51KhdQ0H7K9Gxs8E1J9QPr6dk7OM6k9pgJM8aje8OfNehskBbnxmV0h2e9Q69aCkW2pDOtJdTehRxVAKRnY5hEtLn00QnPf0mgd");
-
+// note that this needs to be a "private" key from STRIPE
+const stripe = require("stripe")(
+  "sk_test_51KhdQ0H7K9Gxs8E1J9QPr6dk7OM6k9pgJM8aje8OfNehskBbnxmV0h2e9Q69aCkW2pDOtJdTehRxVAKRnY5hEtLn00QnPf0mgd"
+);
 module.exports = {
-
-  /**
-   * Retrieve order records.
-   *
-   * @return {Object|Array}
-   */
-
-   find: async ctx => {
-    if (ctx.query._q) {
-      return strapi.services.orders.search(ctx.query);
-    } else {
-      return strapi.services.orders.find(ctx.query);
-    }
-  },
-
-  /**
-   * Retrieve a order record.
-   *
-   * @return {Object}
-   */
-
-  findOne: async ctx => {
-    if (!ctx.params._id.match(/^[0-9a-fA-F]{24}$/)) {
-      return ctx.notFound();
-    }
-
-    return strapi.services.orders.find(ctx.params);
-  },
-
-  /**
-   * Count order records.
-   *
-   * @return {Number}
-   */
-
-  count: async ctx => {
-    return strapi.services.orders.count(ctx.query);
-  },
-
   /**
    * Create a/an order record.
    *
    * @return {Object}
    */
 
-  create: async ctx => {
-
-    const { address, address_line_2, amount, dishes, token, city, state, zip, restaurant_id } = JSON.parse(ctx.request.body);
-
+  create: async (ctx) => {
+    const { address, amount, dishes, token, city, state } = JSON.parse(
+      ctx.request.body
+    );
     const stripeAmount = Math.floor(amount * 100);
-
+    // charge on stripe
     const charge = await stripe.charges.create({
       // Transform cents to dollars.
       amount: stripeAmount,
       currency: "usd",
       description: `Order ${new Date()} by ${ctx.state.user._id}`,
-      source: token
+      source: token,
     });
 
     // Register the order in the database
-    const order = await strapi.services.orders.add({
-      user: ctx.state.user._id,
-      address,
-      address_line_2,
+    const order = await strapi.services.order.create({
+      user: ctx.state.user.id,
+      charge_id: charge.id,
       amount: stripeAmount,
+      address,
       dishes,
       city,
       state,
-      zip,
-      restaurant: restaurant_id,
-      charge_id: charge.id
     });
 
     return order;
   },
-
-  /**
-   * Update a/an order record.
-   *
-   * @return {Object}
-   */
-
-  update: async (ctx, next) => {
-    return strapi.services.orders.edit(ctx.params, ctx.request.body);
-  },
-
-  /**
-   * Destroy a/an order record.
-   *
-   * @return {Object}
-   */
-
-  destroy: async (ctx, next) => {
-    return strapi.services.orders.remove(ctx.params);
-  }
 };
