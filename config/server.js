b20324e3bf28f3e490ca08e0e39cf14a0dbdc712
YannGueguen1 <yanngueguen2015@u.northwestern.edu>
2022-04-18 21:43:40 -0500

    removed url from server

--- config/server.js
+++ config/server.js
@@ -6,5 +6,5 @@ module.exports = ({ env }) => ({
       secret: env('ADMIN_JWT_SECRET', 'edcb569638d7c94f897cab30b9f0817b'),
     },
   },
-  url:'https://7994-168-91-252-63.ngrok.io'
+  // url:'https://7994-168-91-252-63.ngrok.io'
 });
